89519b10edbfb43707a8e4447036610310121d36
sthefanyfalkmoreira-prog <sthefany.falk.moreira@escola.pr.gov.br>
2026-03-17 09:37:47 -0300

    Create desafio 1.py

---
+++ desafio 1.py
@@ -0,0 +1,7 @@
+def somar_dois_numeros():
+    num1 = float(input("Digite o primeiro número: "))
+    num2 = float(input("Digite o segundo número: "))
+    soma = num1 + num2
+    print(f"A soma é: {soma}")
+
+somar_dois_numeros()
