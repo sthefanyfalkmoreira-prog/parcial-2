7dfc525c53ac5911fb1bbe92ff0f58ac304d7972
sthefanyfalkmoreira-prog <sthefany.falk.moreira@escola.pr.gov.br>
2026-03-25 08:36:44 -0300

    desafio 4.py

---
+++ desafio 4.py
@@ -0,0 +1,17 @@
+# Solicita o primeiro número ao usuário e converte para decimal
+n1 = float(input("Digite o primeiro número: "))
+
+# Solicita o segundo número ao usuário e converte para decimal
+n2 = float(input("Digite o segundo número: "))
+
+# Calcula a soma, subtração, multiplicação e divisão dos valores
+soma = n1 + n2
+subtracao = n1 - n2
+multiplicacao = n1 * n2
+divisao = n1 / n2
+
+# Exibe os resultados de cada operação matemática na tela
+print("Soma:", soma)
+print("Subtração:", subtracao)
+print("Multiplicação:", multiplicacao)
+print("Divisão:", divisao)
