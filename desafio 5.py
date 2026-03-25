539924e4a93211f32fbd197675f62aa48a72bc01
sthefanyfalkmoreira-prog <sthefany.falk.moreira@escola.pr.gov.br>
2026-03-25 08:40:41 -0300

    desafio 5.py

--- desafio 5.py
+++ desafio 5.py
@@ -1,17 +1,11 @@
-# Solicita o primeiro número ao usuário e converte para decimal
-n1 = float(input("Digite o primeiro número: "))
+# Solicita o valor da base do triângulo e converte para número decimal
+base = float(input("Digite o valor da base do triângulo: "))
 
-# Solicita o segundo número ao usuário e converte para decimal
-n2 = float(input("Digite o segundo número: "))
+# Solicita o valor da altura do triângulo e converte para número decimal
+altura = float(input("Digite o valor da altura do triângulo: "))
 
-# Calcula a soma, subtração, multiplicação e divisão dos valores
-soma = n1 + n2
-subtracao = n1 - n2
-multiplicacao = n1 * n2
-divisao = n1 / n2
+# Calcula a área multiplicando base por altura e dividindo o resultado por 2
+area = (base * altura) / 2
 
-# Exibe os resultados de cada operação matemática na tela
-print("Soma:", soma)
-print("Subtração:", subtracao)
-print("Multiplicação:", multiplicacao)
-print("Divisão:", divisao)
+# Exibe o resultado final do cálculo da área no console
+print("A área do triângulo é:", area)
