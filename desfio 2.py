69b3e7215bcfa579f896b70155d31ea06e9bb238
sthefanyfalkmoreira-prog <sthefany.falk.moreira@escola.pr.gov.br>
2026-03-25 08:25:52 -0300

    desfio 2.py

--- desfio 2.py
+++ desfio 2.py
@@ -1,12 +1,11 @@
-# Programa que verifica se um número é par ou ímpar
+# 1. Pede para o usuário digitar um número 
+numero = int(input("Digite um número: "))
 
-# Pede para o usuário digitar um número e transforma em inteiro
-num = int(input("Digite um número: "))
-
-# Verifica se o número é par usando o resto da divisão (%)
-if num % 2 == 0:
-    # Se o resto da divisão por 2 for zero, o número é par
+# 2. Testa se o número pode ser dividido por 2 sem sobrar nada
+if numero % 2 == 0:
+    # 3. Se não sobrou nada (resto 0), ele avisa que é PAR
     print("O número é par")
+# 4. Caso contrário (se sobrou 1 no resto da divisão)
 else:
-    # Se não for zero, o número é ímpar
+    # 5. Ele avisa que o número só pode ser ÍMPAR
     print("O número é ímpar")
